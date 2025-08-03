65986b9192ed6643749851b851cac52ab215e97b
jayedra manek <jayedramanek@gmail.com>
2025-08-03 21:28:21 +0530

    Fixed folder name issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import YouTubeGrid from "./components/YouTubeGrid/YouTubeGrid.jsx";
+import YouTubeGrid from "./components/YoutubeGrid/YouTubeGrid.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
